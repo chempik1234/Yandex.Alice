1057b6e23287fe818d99d58de38f2f3d60d20fd3
chempik1234 <dandijar@yandex.ru>
2022-03-12 17:34:25 +0500

    first

---
+++ main.py
@@ -0,0 +1,90 @@
+from flask import Flask, request
+import logging
+import json
+# мы передаём __name__, внутри лога это 'logging
+app = Flask(__name__)
+
+logging.basicConfig(level=logging.INFO)
+
+# Создадим словарь, чтобы для каждой сессии общения
+# с навыком хранились подсказки, которые видел пользователь.
+# Это поможет нам немного разнообразить подсказки ответов
+# (buttons в JSON ответа).
+# Когда новый пользователь напишет нашему навыку,
+# то мы сохраним в этот словарь запись формата
+# sessionStorage[user_id] = {'suggests': ["Не хочу.", "Не буду.", "Отстань!" ]}
+# Такая запись говорит, что мы показали пользователю эти три подсказки.
+# Когда он откажется купить слона,
+# то мы уберем одну подсказку.
+sessionStorage = {}
+
+
+@app.route('/post', methods=['POST'])  # POST для использования json внутри handle_dialog
+def main():
+    logging.info(f'Request: {request.json!r}')
+
+    # Начинаем формировать ответ, согласно документации
+    # мы собираем словарь, который потом при помощи
+    # библиотеки json преобразуем в JSON и отдадим Алисе
+    response = {
+        'session': request.json['session'],
+        'version': request.json['version'],
+        'response': {
+            'end_session': False
+        }
+    }
+    handle_dialog(request.json, response)
+
+    logging.info(f'Response:  {response!r}')
+
+    # Преобразовываем в JSON и возвращаем
+    return json.dumps(response)
+
+
+def handle_dialog(req, res):
+    user_id = req['session']['user_id']
+
+    if req['session']['new']:  # если только зашел то начало игры
+        sessionStorage[user_id] = {
+            'suggests': [
+                "Не хочу.",
+                "Не буду.",
+                "Отстань!",
+            ]
+        }
+        res['response']['text'] = 'Привет! Купи слона!'
+        res['response']['buttons'] = get_suggests(user_id)
+        return
+    # Если он написал 'ладно', 'куплю', 'покупаю', 'хорошо',
+    # то мы считаем, что пользователь согласился.
+    okreq = ['ладно', 'куплю', 'покупаю', 'хорошо']
+    if req['request']['original_utterance'].lower().replace(' ', '') in okreq:
+        res['response']['text'] = 'Слона можно найти на Яндекс.Маркете!'
+        res['response']['end_session'] = True
+        return
+    res['response']['text'] = \
+        f"Все говорят '{req['request']['original_utterance']}', а ты купи слона!"
+    res['response']['buttons'] = get_suggests(user_id)
+
+
+def get_suggests(user_id):  # обычно даёт 2 подсказки из списка,
+    session = sessionStorage[user_id]  # а когда они кончаются, яндекс маркет
+
+    suggests = [
+        {'title': suggest, 'hide': True}
+        for suggest in session['suggests'][:2]
+    ]
+    session['suggests'] = session['suggests'][1:]
+    sessionStorage[user_id] = session
+    # в конце подсказок яндекс маркет
+    if len(suggests) < 2:
+        suggests.append({
+            "title": "Ладно",
+            "url": "https://market.yandex.ru/search?text=слон",
+            "hide": True
+        })
+    return suggests
+
+
+if __name__ == '__main__':
+    app.run()
